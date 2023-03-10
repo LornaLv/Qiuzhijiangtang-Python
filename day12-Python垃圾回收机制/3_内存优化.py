a04df4e5ec79ee91be06689b8614f3939c03cbb0
LornaLv <849475089@qq.com>
2023-03-10 22:24:35 +0800

    2023-03-10

---
+++ day12-Python垃圾回收机制/3_内存优化.py
@@ -0,0 +1,9 @@
+# Date: 2023/3/9 下午3:02
+biga = 100
+bigb = 100
+print(id(biga))
+print(id(bigb))
+del biga
+del bigb
+bigc = 100
+print(id(bigc))
---
+++ day12-Python垃圾回收机制/4_命令行参数.py
@@ -0,0 +1,5 @@
+# Date: 2023/3/9 下午3:19
+import sys
+
+print('参数个数为：', len(sys.argv))
+print('参数列表：', str(sys.argv))
